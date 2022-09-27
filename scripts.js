c8e2323127c08c0622acc85c23b12f92b238ef55
João Pedro <jp.santos1008@gmail.com>
2022-09-27 07:59:21 -0300

    Alterando configurações do efeito bolha e corrigindo o position dos cards

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 function criarBolhas(){
-    const section = document.querySelector('section');
+    const section = document.querySelector('header');
     const criaElemento = document.createElement('span');
 
-    var tamanho = Math.random() * 40;
+    var tamanho = Math.random() * 60;
 
     criaElemento.style.width = tamanho + 'px';
     criaElemento.style.height = tamanho + 'px';
-    criaElemento.style.left = Math.random() * innerWidth + 'px';
+    criaElemento.style.left = Math.random() * (innerWidth/1.25) + 'px';
     section.appendChild(criaElemento);
 
     setTimeout(()=> {
         criaElemento.remove();
-    },400);
+    },850);
 }
 
-setInterval(criarBolhas, 70);
+setInterval(criarBolhas, 150);
